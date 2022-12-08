0b5b19346f4b350f6e846d026b8b4281eb3f983b
lucas-buccilli <lucas.buccilli@willowtreeapps.com>
2022-12-07 22:48:35 -0500

    Complete day 6

---
+++ AOC/Days/Day6/Day6.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+using Days;
+
+namespace Day6
+{
+    public class Executor : Day
+    {
+        public Executor()
+        {
+        }
+
+        public override string part1(string input)
+        {
+            var chars = input.ToCharArray();
+            for (var i = 3; i < chars.Length; i++)
+            {
+                var code = new HashSet<char>()
+                {
+                    chars[i],
+                    chars[i - 1],
+                    chars[i - 2],
+                    chars[i - 3]
+                };
+                if (code.Count == 4)
+                {
+                    return (i + 1) .ToString();
+                }
+                
+            }
+            
+
+            return chars.Length.ToString();
+        }
+
+        public override string part2(string input)
+        {
+            var chars = input.ToCharArray();
+            for (var i = 13; i < chars.Length; i++)
+            {
+                var code = new HashSet<char>();
+
+                for (var j = 0; j < 14; j++)
+                {
+                    code.Add(chars[i - j]);
+                }
+                if (code.Count == 14)
+                {
+                    return (i + 1).ToString();
+                }
+
+            }
+
+
+            return chars.Length.ToString();
+        }
+    }
+}
+
--- AOC/Main.cs
+++ AOC/Main.cs
@@ -11,7 +11,8 @@ namespace AOC
             {"day2", new Day2.Executor()},
             {"day3", new Day3.Executor()},
             {"day4", new Day4.Executor()},
-            {"day5", new Day5.Executor()}
+            {"day5", new Day5.Executor()},
+            {"day6", new Day6.Executor()}
         };
 
         static void Main(string[] args)
